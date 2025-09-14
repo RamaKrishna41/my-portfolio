e6668b7dfe44e53cd0f900129cc98a754ff136a9
RamaKrishna41 <ramakrishna665577@gmail.com>
2025-09-14 16:21:07 -0400

    Adding Projects

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.home.apps.HomeConfig',
+    'apps.projects.apps.ProjectsConfig',
 ]
 
 MIDDLEWARE = [
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('apps.home.urls')),    # Home Page
+    path('projects/', include('apps.projects.urls')), # Projects Page
     path('admin/', admin.site.urls),
 ]
